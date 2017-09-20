87ed40ad3182f724f3997be91a05a89c2d1cd889
Shiv Prakash Anand <shivakp2111@gmail.com>
2017-09-20 16:54:44 +0530

    Create 805A.cpp

---
+++ codeforces/805A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int l,r;
+    scanf("%lld%lld", &l, &r);
+
+    if(l == r)
+    {
+        cout<<l<<endl;
+        return 0;
+    }
+
+    cout<<"2"<<endl;
+    return 0;
+}
