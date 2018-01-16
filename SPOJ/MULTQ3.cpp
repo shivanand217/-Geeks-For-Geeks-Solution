9fd55a4688e5ce6be4dc266aa32d03f2ff850430
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-01-16 23:00:04 +0530

    Update MULTQ3.cpp

--- SPOJ/MULTQ3.cpp
+++ SPOJ/MULTQ3.cpp
@@ -1,5 +1,27 @@
 #include <bits/stdc++.h>
+
+#define mp make_pair
+#define ll long long
+#define pb push_back
+
+#define ss second
+#define ff first
+#define si(x) scanf("%d",&x)
+#define slli(x) scanf("%lld",&x)
+#define pi(x) printf("%d",x)
+
+#define mx5 100005
+#define mx6 1000006
+#define mod 1000000007
+
+#define rep(i,n) for(int i=0; i<n; i++)
+#define fast std::ios::sync_with_stdio(false)
+#define gc() getchar()
+#define pc(x) putchar(x)
+
 using namespace std;
+
+typedef pair<int,int> pii;
 
 const int mxx = 100005;
 // special value of infinity to take for getting rid of overflows
