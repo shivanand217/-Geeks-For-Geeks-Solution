8a77078818f45f27f30b5c6997c2ec363d5add5e
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-02-10 01:57:32 +0530

    commit

---
+++ codeforces/922B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define ll long long
+#define pb push_back
+
+#define ss second
+#define ff first
+#define si(x) scanf("%d",&x)
+#define slli(x) scanf("%lld",&x)
+#define pi(x) printf("%d",x)
+
+#define mx5 100005
+#define mx6 1000006
+#define mod 1000000007
+
+#define rep(i,n) for(int i=0; i<n; i++)
+#define fast std::ios::sync_with_stdio(false)
+#define gc() getchar()
+#define pc(x) putchar(x)
+
+using namespace std;
+
+typedef pair<int,int> pii;
+
+const int mxx = 100001;
+// special value of infinity to take for getting rid of overflows
+const int inf = 0x3f3f3f3f;
+
+/******************* Problem Code *****************/
+bool isdegenerate(int x,int y,int z){
+    if(x+y>z && z+x>y && x+z>y)
+        return true;
+    return false;
+}
+int main() {
+    fast;
+    int n;
+    cin>>n;
+    int ans=0;
+    for(int i=1; i < n; i++) {
+        for(int j=i+1; j<=n; j++) {
+            int xo = (i^j);
+            if(xo <= n && (xo != i && xo != j )) {
+                if(isdegenerate(i,j,xo)) {
+                    ans++;
+                }
+            }
+        }
+    }
+    ans/=3;
+    cout<<ans<<endl;
+    return 0;
+}
