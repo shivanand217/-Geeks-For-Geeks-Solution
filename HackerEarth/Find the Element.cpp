a8e3c59d9b06022a9ede033654c3c4e869af5a09
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-03-15 15:59:15 +0530

    15-03-2018

--- HackerEarth/Find the Element.cpp
+++ HackerEarth/Find the Element.cpp
@@ -15,17 +15,15 @@ int main() {
     string s,qu;
     int direction;
     /**
-        direction-0 = right
-        direction-1 = down
-        direction-2 = left
-        direction-3 = up
+        direction- 0 = right
+        direction- 1 = down
+        direction- 2 = left
+        direction- 3 = up
     **/
     int right_limit,down_limit,left_limit,up_limit,curr_cell_row,curr_cell_col;
-    string ans;
     vector<string> v;
     while(t--) {
         v.clear();
-        ans = "";
         direction = 0;
         cin>>n>>m;
         right_limit=m-1;
@@ -50,18 +48,18 @@ int main() {
                     inc++;
                     if(en[curr_cell_row][curr_cell_col] == qu) {
                         v.push_back(en[curr_cell_row][curr_cell_col]);
-                        ans += en[curr_cell_row][curr_cell_col];
                         break;
                     } else {
-                        ans += en[curr_cell_row][curr_cell_col];
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
                         curr_cell_col++;
                     }
                 }
-                if(en[curr_cell_row][curr_cell_col] == qu) {
-                    break;
-                }
                 if(curr_cell_col > right_limit)
                     curr_cell_col--;
+                if(en[curr_cell_row][curr_cell_col] == qu) {
+                    v.push_back(en[curr_cell_row][curr_cell_col]);
+                    break;
+                }
                 direction++;
                 direction %= 4;
                 right_limit--;
@@ -72,15 +70,19 @@ int main() {
                 while(curr_cell_row <= down_limit) {
                     inc++;
                     if(en[curr_cell_row][curr_cell_col] == qu) {
-                        ans += en[curr_cell_row][curr_cell_col];
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
                         break;
                     } else {
-                        ans += en[curr_cell_row][curr_cell_col];
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
                         curr_cell_row++;
                     }
                 }
                 if(curr_cell_row > down_limit)
                     curr_cell_row--;
+                if(en[curr_cell_row][curr_cell_col] == qu) {
+                    v.push_back(en[curr_cell_row][curr_cell_col]);
+                    break;
+                }
                 direction++;
                 direction %= 4;
                 down_limit--;
@@ -91,15 +93,19 @@ int main() {
                 while(curr_cell_col >= left_limit) {
                     inc++;
                     if(en[curr_cell_row][curr_cell_col] == qu) {
-                        ans += en[curr_cell_row][curr_cell_col];
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
                         break;
                     } else {
-                        ans += en[curr_cell_row][curr_cell_col];
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
                         curr_cell_col--;
                     }
                 }
                 if(curr_cell_col < left_limit)
                     curr_cell_col++;
+                if(en[curr_cell_row][curr_cell_col] == qu) {
+                    v.push_back(qu);
+                    break;
+                }
                 direction++;
                 direction %= 4;
                 left_limit++;
@@ -110,29 +116,31 @@ int main() {
                 while(curr_cell_row >= up_limit) {
                     inc++;
                     if(en[curr_cell_row][curr_cell_col] == qu) {
-                        ans += en[curr_cell_row][curr_cell_col];
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
                         break;
+                    } else {
+                        v.push_back(en[curr_cell_row][curr_cell_col]);
+                        curr_cell_row--;
                     }
-                    ans += en[curr_cell_row][curr_cell_col];
-                    curr_cell_row--;
                 }
                 if(curr_cell_row < up_limit)
                     curr_cell_row++;
+                if(en[curr_cell_row][curr_cell_col] == qu) {
+                    v.push_back(qu);
+                    break;
+                }
                 direction++;
                 direction %= 4;
                 up_limit++;
                 curr_cell_col++;
             }
-            //cout<<curr_cell_row<<" "<<curr_cell_col<<endl;
             if(inc == 0)
                 break;
         }
-        cout<<ans<<endl;
-        /**if(ans.length() > 0) {
-            cout<<ans<<endl;
-        } else {
-            cout<<"NO"<<endl;
-        }**/
+        int len=v.size()-1;
+        for(auto it: v)
+            cout<<it;
+        cout<<endl;
     }
 
     return 0;
