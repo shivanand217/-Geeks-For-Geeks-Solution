d2c50089f48d400e926d9583fc5ad540485d826c
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-06-07 23:05:29 +0530

    new problem

---
+++ codeforces/965A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast std::ios::sync_with_stdio(false)
+int k,n,s,p;
+
+int main() {
+    fast;
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin>>k>>n>>s>>p;
+
+    int sheets,packs,total_sheets;
+
+    if(n%s == 0) {
+        sheets = n/s;
+    } else {
+        sheets = n/s + 1;
+    }
+
+    total_sheets=k*sheets;
+
+    if(total_sheets%p == 0) {
+        packs=total_sheets/p;
+    } else {
+        packs=total_sheets/p+1;
+    }
+
+    cout<<packs<<endl;
+
+    return 0;
+}
