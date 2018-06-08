c2e1bc218c910bc8df1ff075129a5d2118dfd6ff
Shiv Prakash Anand <shivakp2111@gmail.com>
2018-06-08 09:43:08 +0530

    new

---
+++ codeforces/problem965A.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class problem965A {
+
+    static int k=0,n=0,s=0,p=0;
+    static int sheets,packs,total_sheets;
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+
+        k = Integer.parseInt(str[0]);
+        n = Integer.parseInt(str[1]);
+        s = Integer.parseInt(str[2]);
+        p = Integer.parseInt(str[3]);
+
+        if(n%s == 0) {
+            sheets = n/s;
+        } else {
+            sheets = n/s + 1;
+        }
+
+        total_sheets = k*sheets;
+
+        if(total_sheets%p == 0){
+            packs=total_sheets/p;
+        } else {
+            packs=total_sheets/p+1;
+        }
+
+        System.out.println(packs);
+    }
+}
