673ccd7c00682936c3df8af57b83234027d196f1
shiv anand <shivakp2111@gmail.com>
2018-11-19 12:19:30 +0530

    second problem

---
+++ codeforces/1077B.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+#define maxx 1005
+
+int arr[maxx];
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+
+    int j=1;
+    int ans=0;
+
+    while(j<n){
+        if(arr[j] == 0){
+            if(arr[j-1]==1&&arr[j+1]==1){
+                arr[j+1]=0;
+                ans++;    
+            }
+        } j++;
+    }
+    cout<<ans<<endl;
+
+    return 0;
+}
