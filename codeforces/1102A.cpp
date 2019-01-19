7e2e3e28969b90eb2c4adf8544f543aa5e758d97
shiv anand <shivakp2111@gmail.com>
2019-01-19 12:35:51 +0530

    after long time

---
+++ codeforces/1102A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+    if(n%4 == 0 || n%4 == 3){
+        cout<<"0"<<endl;
+    } else {
+        cout<<"1"<<endl;
+    }
+    return 0;
+}
