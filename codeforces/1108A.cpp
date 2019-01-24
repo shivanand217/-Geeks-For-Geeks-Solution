d1b5abd90a4424a4f41b4b415c9b99ddabd6f878
shiv anand <shivakp2111@gmail.com>
2019-01-24 23:59:05 +0530

    ne

---
+++ codeforces/1108A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int q;
+    cin>>q;
+
+    int l1,l2,r1,r2;
+    
+    while(q--){
+        cin>>l1>>r1>>l2>>r2;
+        cout<<l1<<" "<<r2<<endl;
+    }
+    return 0;
+}
