35a2e2d234514920b7dddccd2991070f8ddb19e3
shiv anand <shivakp2111@gmail.com>
2019-04-26 14:40:33 +0530

    1151B

---
+++ codeforces/1151B.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,a[505][505],b[505][505],c[505];
+void pr()
+{
+	cout<<"TAK"<<endl;
+	for(int i=1;i<=n;i++)
+	cout<<c[i]<<" ";
+	exit(0);
+}
+int main()
+{
+	cin>>n>>m;
+	int i,j,sum=0;
+	for(i=1;i<=n;i++)
+	for(j=1;j<=m;j++) 
+	{
+		cin>>a[i][j];
+	}
+	for(i=1;i<=n;i++) sum^=a[i][1],c[i]=1;
+	if(sum!=0) pr();
+	for(i=1;i<=n;i++)
+	for(j=2;j<=m;j++)
+	{
+		if(a[i][j]!=a[i][1]) {c[i]=j;pr();} 
+	}
+	cout<<"NIE"<<endl;
+}
